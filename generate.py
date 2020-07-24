840c9b18b008cf3e169920571825eb6d17bbb29f
Michael Patel <michael.patel94@gmail.com>
2020-07-24 14:56:39 -0400

    Successfully generates pics of tweets

--- generate.py
+++ generate.py
@@ -29,9 +29,18 @@ from parameters import *
 
 
 ################################################################################
+# get specified element
+def get_element(id):
+    elem = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, id))
+    )
+
+    return elem
+
+
 # generate tweet image
 def generate_tweet():
-
+    print()
 
 # generate text output
 def generate_text(model, input_string):
@@ -45,7 +54,7 @@ def generate_text(model, input_string):
 
     model.reset_states()
 
-    num_gen_char = np.random.randint(140, 280)
+    num_gen_char = np.random.randint(40, 280)
 
     for i in range(num_gen_char):
         predictions = model(input_seq)
@@ -86,3 +95,91 @@ if __name__ == "__main__":
         input_string=START_STRING
     )
     print(f'Generated message: {generated_message}')
+
+    # chromedriver
+    chromedriver_filepath = os.path.join(GENERATED_DIR, "chromedriver.exe")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(
+        chromedriver_filepath,
+        options=chrome_options
+    )
+
+    # open url
+    url = "https://www.tweetgen.com/create/tweet.html"
+    driver.get(url=url)
+
+    # Theme (light)
+
+    # Profile Picture
+    profile_pic_filepath = os.path.join(GENERATED_DIR, "trump_profile.jpg")
+    profile_pic_upload = get_element(id="pfpInput")
+    profile_pic_upload.send_keys(profile_pic_filepath)
+
+    # Name
+    name = "Donald J. Trump"
+    name_element = get_element(id="nameInput")
+    name_element.send_keys(name)
+
+    # Username
+    username = "realDonaldTrump"
+    username_element = get_element(id="usernameInput")
+    username_element.send_keys(username)
+
+    # Verified User
+    verify_checkbox = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div[1]/form/div[6]/div/label"))
+    )
+    verify_checkbox.click()
+
+    # Tweet Content
+    tweet_content = generated_message
+    tweet_box = get_element(id="tweetTextInput")
+    tweet_box.send_keys(tweet_content)
+
+    # Image (skip)
+
+    # Time
+    time = datetime.now().strftime("%H:%M")
+    time_element = get_element(id="time")
+    time_element.send_keys(time)
+
+    # Date
+    # Day
+    day = datetime.now().strftime("%d")
+    day_element = get_element(id="dayInput")
+    day_element.send_keys(day)
+
+    # Month
+    month = datetime.now().strftime("%m")
+    month_element = get_element(id="monthInput")
+    month_element.send_keys(month)
+
+    # Year
+    year = datetime.now().strftime("%Y")
+    year_element = get_element(id="yearInput")
+    year_element.send_keys(year)
+
+    # Retweets
+    randomize_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div[1]/form/div[11]/div[2]/button"))
+    )
+    randomize_button.click()
+
+    # Likes
+
+    # Client (skip)
+
+    # Generate Image and Download
+    generate_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="downloadButton"]'))
+    )
+    generate_button.click()
+
+    # download generated image
+    sleep(1)
+    gen_image = get_element(id="imageOutput")
+    src = gen_image.get_attribute("src")
+    urllib.request.urlretrieve(src, os.path.join(GENERATED_DIR, "generated.png"))
+
+    driver.close()
