911555bc023ba583f142beae74e53c137e13da59
Michael Patel <michael.patel94@gmail.com>
2020-07-27 13:00:12 -0400

    Training bidirectional 10 epochs

--- parameters.py
+++ parameters.py
@@ -30,7 +30,7 @@ from selenium.webdriver.support import expected_conditions as EC
 ################################################################################
 TWEET_LENGTH = 280
 
-NUM_EPOCHS = 6
+NUM_EPOCHS = 10
 BUFFER_SIZE = 10000
 BATCH_SIZE = 64
 EMBEDDING_DIM = 256
